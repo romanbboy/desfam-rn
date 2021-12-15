a62f3dc0925bc85414b5c3c4f8e3c58060b80dac
r.moroz <r.moroz@filanco.ru>
2021-12-15 13:43:33 +0300

    status bar

--- App.js
+++ App.js
@@ -8,6 +8,7 @@ import * as eva from '@eva-design/eva';
 import {ApplicationProvider, IconRegistry, Layout, Text} from '@ui-kitten/components';
 import {EvaIconsPack} from "@ui-kitten/eva-icons";
 import MyToast from "./src/components/toast";
+import {StatusBar} from "react-native";
 
 const App = () => {
   const [isReady, setIsReady] = useState(false)
@@ -25,6 +26,7 @@ const App = () => {
   return (
     <>
       <IconRegistry icons={EvaIconsPack} />
+      <StatusBar barStyle='dark-content' animated={true} />
       <ApplicationProvider {...eva} theme={eva.light}>
         <Provider store={store}>
           <AppNavigation />
